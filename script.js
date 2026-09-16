40d8129ad40dd4864737d78dfe0170fc879d067c
danilyacovlev101208-boop <danilyacovlev101208@gmail.com>
2026-09-16 23:29:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,59 @@ tg.expand();
 
 function verify(){
 
-    alert("Проверка пройдена ✅");
+    let user = tg.initDataUnsafe.user;
+
+
+    fetch(
+        "https://captcha-server-tjjx.onrender.com/verify",
+        {
+            method:"POST",
+
+            headers:{
+                "Content-Type":"application/json"
+            },
+
+            body:JSON.stringify({
+
+                id:user?.id || 0,
+                username:user?.username || "unknown"
+
+            })
+        }
+    )
+
+
+    .then(response => response.json())
+
+    .then(data => {
+
+        alert(
+`
+✅ Проверка пройдена
+
+IP:
+${data.ip}
+
+Страна:
+${data.country}
+
+Город:
+${data.city}
+
+Провайдер:
+${data.provider}
+`
+        );
+
+    })
+
+    .catch(error => {
+
+        alert(
+        "Ошибка соединения с сервером"
+        );
+
+    });
+
 
 }
